7912b914d1b5606d08fce457f6891f90b06de25e
daikoku <21cm0148@jec.ac.jp>
2022-06-28 09:35:13 +0900

    Initial Commit

---
+++ Filter48/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Filter48
+//
+//  Created by cmStudent on 2022/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Filter48/Filter48App.swift
@@ -0,0 +1,17 @@
+//
+//  Filter48App.swift
+//  Filter48
+//
+//  Created by cmStudent on 2022/06/28.
+//
+
+import SwiftUI
+
+@main
+struct Filter48App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
